91645f8016903c58cdce3572aaa6cd84b5649caf
Gabriel Paoliello <ga.paoliello@gmail.com>
2023-04-09 23:25:17 -0300

    Initial commit

---
+++ Especies.py
@@ -0,0 +1,81 @@
+import random
+from Consts import STEP_SIZE, SCR_WIDTH, SCR_HEIGHT
+from pygame import Surface
+import pygame
+from math import sin, cos, radians
+
+class Especie:
+    def __init__(self, sense, speed, angle, age, pos) -> None:
+        self.sense:int = sense
+        self.speed:int = speed
+        self.angle:int = angle
+        self.age:int = age
+        self.pos:tuple(int,int) = pos
+
+    def get_pos_x(self) -> int:
+        return self.pos[0]
+
+    def get_pos_y(self) -> int:
+        return self.pos[1]
+
+    def get_sense(self) -> int:
+        return self.sense
+        
+    def get_speed(self) -> int:
+        return self.speed
+
+    def get_angle(self) -> int:
+        return self.angle
+
+    def get_age(self) -> int:
+        return self.age
+
+    def set_angle(self, angle) -> None:
+        self.angle = angle
+        return None
+
+    def walk(self) -> None:
+        pass
+
+    def draw_self(self, sense_surfice: Surface, species_surfice: Surface):
+        pass
+
+class EspecieX(Especie):
+    def __init__(self) -> None:
+        super().__init__(
+            sense = random.randint(1,3),
+            speed = random.randint(1,3),
+            angle = random.randint(0,359),
+            age = 0,
+            pos = (random.randint(20, SCR_WIDTH), random.randint(20, SCR_HEIGHT))
+        )
+
+    def walk(self):
+        if (self.get_pos_y() >= SCR_HEIGHT or self.get_pos_y() <= 0 or
+            self.get_pos_x() >= SCR_WIDTH or self.get_pos_x() <= 0):
+            self.set_angle(self.get_angle() + 180)
+        else:
+            var_angle = random.randint(-5,5)
+            self.set_angle(self.get_angle() + var_angle)
+
+        angle_radians = radians(self.get_angle())
+        step_x = cos(angle_radians)*self.get_speed()*STEP_SIZE
+        step_y = sin(angle_radians)*self.get_speed()*STEP_SIZE
+        self.pos = (self.get_pos_x() + step_x, self.get_pos_y() + step_y)
+        
+
+    def draw_self(self, sense_surfice: Surface, species_surfice: Surface):
+        # Sentido
+        pygame.draw.circle(sense_surfice, (217,217,217), radius=40*self.get_sense(), center=(self.get_pos_x(), self.get_pos_y())) 
+
+        # Individuo
+        pygame.draw.circle(species_surfice, (40*self.get_speed(), 40*self.get_sense(), 255-40*self.get_age()), radius=10, center=(self.get_pos_x(), self.get_pos_y()))
+
+class EspecieFactory():
+    def make_especie_list() -> list:
+        # especie = random.choice({EspecieX.__class__, })
+        individuos:list = []
+        for x in range(0, 10):
+            individuos.append(EspecieX())
+        return individuos
+                
---
+++ Evolve.py
@@ -0,0 +1,54 @@
+
+
+
+import pygame
+import random
+from Consts import SCR_HEIGHT, SCR_WIDTH
+from Especies import Especie, EspecieFactory
+from pygame import Surface
+
+def main():
+    # Import and initialize the pygame library
+    pygame.init()
+    # Set up the drawing window
+    screen: Surface = pygame.display.set_mode((SCR_WIDTH, SCR_HEIGHT))
+    
+
+    individuos:list(Especie) = EspecieFactory.make_especie_list()
+    # Run until the user asks to quit
+    running = True
+    while running:
+        # Did the user click the window close button?
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+
+                running = False
+        process(individuos)
+        print_screen(screen, individuos)
+
+    # Done! Time to quit.
+    pygame.quit()
+
+def process(individuos:list):
+    for individuo in individuos:
+        individuo: Especie = individuo
+        individuo.walk()
+
+def print_screen(screen: Surface, individuos:list):
+    
+    species_surfice = pygame.Surface((SCR_WIDTH, SCR_HEIGHT),pygame.SRCALPHA, 32)
+    species_surfice = species_surfice.convert_alpha()
+    # Fill the background with white
+    screen.fill((255, 255, 255))
+    for individuo in individuos:
+        individuo: Especie = individuo
+        individuo.draw_self(screen, species_surfice)
+        screen.blit(species_surfice, (0,0))
+        #pygame.draw.circle(screen, (random.randint(0,255), random.randint(0,255), 255), radius=10, center=(individuo.get_pos_x(), individuo.get_pos_y()))
+
+    
+    # Flip the display
+    pygame.display.flip()
+
+main()
