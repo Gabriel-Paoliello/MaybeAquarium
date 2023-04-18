99d4ef00834e9a7fc0330b045db3e36a1b4301be
Arthur Moreira Passos <arthurmoreirap@gmail.com>
2023-04-18 00:45:21 -0300

    implemented drawer

--- lib/views/Drawer.py
+++ lib/views/Drawer.py
@@ -14,22 +14,43 @@ class Drawer():
         # Set up the drawing window
         self.screen: pygame.Surface = pygame.display.set_mode((SCR_WIDTH, SCR_HEIGHT))
     
-    def draw_entities(self, entities: list):
+    def draw_entities_all(self, foods: list, specimens: list):
+        entities_surface = pygame.Surface((SCR_WIDTH, SCR_HEIGHT),pygame.SRCALPHA, 32)
+        entities_surface = entities_surface.convert_alpha()
+        # Fill the background with white
+        self.screen.fill((255, 255, 255))
+        
         for specimen in specimens:
             specimen: Specimen = specimen
-            specimen.draw_self(screen, specimens_surface)
+            self.__draw_specimen(specimen, entities_surface, self.screen)
             
         for food in foods:
-            food.draw_self(specimens_surface)
-        screen.blit(specimens_surface, (0,0))
+            self.__draw_food(food, entities_surface)
+            
+        self.screen.blit(entities_surface, (0,0))
         
-    def draw_specimens_
+        # Flip the display
+        pygame.display.flip()
+        
+    # def draw_entities(self, entities: list):
+    #     entities_surface = pygame.Surface((SCR_WIDTH, SCR_HEIGHT),pygame.SRCALPHA, 32)
+    #     entities_surface = entities_surface.convert_alpha()
+    #     # Fill the background with white
+    #     self.screen.fill((255, 255, 255))
+        
+    #     for entity in entities:
+    #         self.__draw_entity(entity, entities_surface)
+            
+    #     self.screen.blit(entities_surface, (0,0))
+        
+    #     # Flip the display
+    #     pygame.display.flip()
     
-    def __draw_entity(self, entity: Entity, entities_surface: Surface, secondary_surface: Surface ):
-        if isinstance(entity, Specimen):
-            self.__draw_specimen(entity, entities_surface, secondary_surface)
-        elif isinstance(entity, Food):
-            self.__draw_food(entity, entities_surface)
+    # def __draw_entity(self, entity: Entity, entities_surface: Surface):
+    #     if isinstance(entity, Specimen):
+    #         self.__draw_specimen(entity, entities_surface, self.screen)
+    #     elif isinstance(entity, Food):
+    #         self.__draw_food(entity, entities_surface)
             
     def __draw_specimen(self, specimen: Specimen, specimens_surface: Surface, sense_surface: Surface ):
         # Sentido
@@ -43,5 +64,5 @@ class Drawer():
 
         pygame.draw.line(sense_surface, (0,0,0), specimen.get_pos_tuple(), (point_x, point_y))
     
-    def __draw_food(specimen, food: Food, specimens_surface: Surface):
+    def __draw_food(self, food: Food, specimens_surface: Surface):
         pygame.draw.circle(specimens_surface, (246,190,0), radius=food.body_radius, center=(food.get_pos_x(), food.get_pos_y())) 
